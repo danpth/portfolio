6de2e89bdeaf162d99cd7f42bf8b8a5fe88f67e0
Daniel Choma <66999833+danpth@users.noreply.github.com>
2023-07-05 10:40:40 +0200

    About-Exp
    About and Experience sections created

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './about.css'
-
+import ME from '../../assets/fotocv.png'
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ex totam temporibus repudiandae suscipit voluptas placeat a numquam laudantium vero. Rem sed eaque nulla quo modi veniam quis vitae quaerat repudiandae sit autem esse molestiae illum corrupti beatae temporibus at error asperiores cum omnis, tempore ex doloribus qui laboriosam. Ipsum.</p>
+          <a href="#contacts" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+
+
+        <div className="work__experience">
+          <h3>Work Experience</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Java</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Spring Boot</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>SQL</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Git</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Intellij</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Postman</h4>
+            </article>
+          </div>
+        </div>
+
+
+        <div className="personal__experience">
+          <h3>Personal Experience</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>C</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>HTML CSS JS</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Basic React</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Python</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Unity & C#</h4>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <h4>Quarkus</h4>
+            </article>
+          </div>
+        </div>
+
+
+      </div>
     </section>
   )
 }
