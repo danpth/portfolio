00208905660ca9d00b278ad4f8c684a778798744
Daniel Choma <66999833+danpth@users.noreply.github.com>
2023-07-06 19:01:33 +0200

    portfolio
    portfolio section added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/img1.avif'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/danpth/',
+    demo: 'https://github.com/danpth/'
+  }
+]
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title}/>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+        
+      </div>
     </section>
   )
 }
